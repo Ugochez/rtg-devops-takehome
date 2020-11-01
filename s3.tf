ce6728fdfe42e90c4da0b8f567ad73a2f880eabf
Ugo Mbagwu <ugo@Ugos-MacBook-Air.local>
2020-11-01 18:51:46 -0500

    adding s3.tf to remote repo

---
+++ s3.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"
+    key    = "gatsby-bucket-rtg.tfstate"
+    region = "us-east-2"
+  }
+}
+
+resource "aws_s3_bucket" "buckettzz" {
+  bucket = "gatsby-bucket-rtg"
+  acl    = "public-read"
+
+  policy = <<POLICY
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::jkrsp.com/*"
+        }
+      ]
+    }
+  POLICY
+
+  website {
+    index_document = "index.html"
+  }
+}
